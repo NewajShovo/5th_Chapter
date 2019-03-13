5bbf70305f7492842f18c12912f6d09693bc7e7d
Shafiq Shovo <shovo@Shafiqs-MacBook-Pro.local>
2019-03-13 19:47:17 +0600

    all are added

---
+++ BNRHypnosisView.m
@@ -0,0 +1,21 @@
+//
+//  BNRHypnosisView.m
+//  Hypnosister
+//
+//  Created by Shafiq Shovo on 13/3/19.
+//  Copyright © 2019 Shafiq Shovo. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by Shafiq Shovo on 13/3/19.
+//  Copyright © 2019 Shafiq Shovo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
